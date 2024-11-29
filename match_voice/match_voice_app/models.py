6202bc6ce2e43d97e28fcc153330fb0b2c0ac7ad
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2024-11-29 11:02:56 +0200

    Create models with names Tournament,Commentator,Match in models.py

--- match_voice/match_voice_app/models.py
+++ match_voice/match_voice_app/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    info = models.TextField(blank=True, null=True)
+    tournament_image = models.ImageField(upload_to="tournament_images", blank=True, null=True,)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Commentator(AbstractUser):
+    years_of_experience = models.IntegerField(null=True)
+
+    class Meta:
+        verbose_name = "commentator"
+        verbose_name_plural = "commentators"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Match(models.Model):
+    title = models.CharField(max_length=255)
+    match_date = models.DateField(blank=True, null=True)
+    match_image = models.ImageField(upload_to="tournament_images", blank=True, null=True)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    commentators = models.ManyToManyField(Commentator, related_name="matches")
+
+    def __str__(self):
+        return self.title
