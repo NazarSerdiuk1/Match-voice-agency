1fc5fe73e469154a17e364ad9a61b2dab5bf5f96
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2024-12-01 16:55:31 +0200

    Create first view and modify urls.py.Create base.html

--- match_voice/match_voice_app/views.py
+++ match_voice/match_voice_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        "title": "MatchVoice"
+    }
+    return render(request, "match_voice/index.html", context=context)
