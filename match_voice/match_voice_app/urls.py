77098f8cbae5f7ceebd6774c114b21489cb61070
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2024-12-01 20:23:19 +0200

    Update settings.py.Create tournament_list view and tournaments page

--- match_voice/match_voice_app/urls.py
+++ match_voice/match_voice_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import index, search_view
+from .views import index, search_view, tournament_list
 
 app_name = "match_voice"
 
 urlpatterns = [
     path("", index, name="index"),
     path("search/", search_view, name="search"),
-
+    path("tournaments/", tournament_list, name="tournament-list"),
 ]
--- match_voice/match_voice_app/views.py
+++ match_voice/match_voice_app/views.py
@@ -1,11 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from .models import Tournament,Match,Commentator
+from .models import Tournament, Match, Commentator
 
 
 def index(request):
-    context = {
-        "title": "MatchVoice"
-    }
+    context = {"title": "MatchVoice"}
     return render(request, "match_voice/index.html", context=context)
 
 
@@ -14,15 +13,31 @@ def search_view(request):
     results = None
 
     if query:
-        results = list(
-            Tournament.objects.filter(name__icontains=query)
-        ) + list(
-            Commentator.objects.filter(
-                username__icontains=query
-            ) | Commentator.objects.filter(first_name__icontains=query) | Commentator.objects.filter(
-                last_name__icontains=query)
-        ) + list(
-            Match.objects.filter(title__icontains=query)
+        results = (
+            list(Tournament.objects.filter(name__icontains=query))
+            + list(
+                Commentator.objects.filter(username__icontains=query)
+                | Commentator.objects.filter(first_name__icontains=query)
+                | Commentator.objects.filter(last_name__icontains=query)
+            )
+            + list(Match.objects.filter(title__icontains=query))
         )
 
-    return render(request, "match_voice/search.html", {"query": query, "results": results})
+    return render(
+        request, "match_voice/search.html", {"query": query, "results": results}
+    )
+
+
+def tournament_list(request):
+    tournaments = Tournament.objects.all()
+    paginate_by = 3
+
+    paginator = Paginator(tournaments, paginate_by)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "tournaments" : tournaments,
+        "page_obj": page_obj,
+    }
+    return render(request, "match_voice/tournament_list.html", context=context)
