7e5d8818da102dc37a1950877b626ddeecfe8d3f
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2024-12-01 17:33:44 +0200

    Create search form

--- match_voice/match_voice_app/urls.py
+++ match_voice/match_voice_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, search_view
 
 app_name = "match_voice"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("search/", search_view, name="search"),
+
 ]
--- match_voice/match_voice_app/views.py
+++ match_voice/match_voice_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Tournament,Match,Commentator
 
 
 def index(request):
@@ -6,3 +7,22 @@ def index(request):
         "title": "MatchVoice"
     }
     return render(request, "match_voice/index.html", context=context)
+
+
+def search_view(request):
+    query = request.GET.get("query", "")
+    results = None
+
+    if query:
+        results = list(
+            Tournament.objects.filter(name__icontains=query)
+        ) + list(
+            Commentator.objects.filter(
+                username__icontains=query
+            ) | Commentator.objects.filter(first_name__icontains=query) | Commentator.objects.filter(
+                last_name__icontains=query)
+        ) + list(
+            Match.objects.filter(title__icontains=query)
+        )
+
+    return render(request, "match_voice/search.html", {"query": query, "results": results})
