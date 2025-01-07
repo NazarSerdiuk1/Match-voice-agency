0d468cec36229857146419572fff139422a268b8
Nazar Serdyuk <nazarserdyuk122003@gmail.com>
2025-01-07 18:24:05 +0200

    Modify settings and env.sample

--- match_voice/match_voice/settings/prod.py
+++ match_voice/match_voice/settings/prod.py
@@ -1,10 +1,10 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Database
